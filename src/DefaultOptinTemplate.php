00e7e8ba8108ce4d1f70a23881381aee63e2bce5
Rob Gongora <roberto@tri.be>
2022-09-25 18:02:57 +0200

    feat: refactored how default optin template is rendered.
    This is more aligned to how I imagine a WordPress developer would want to initialize each part of the library.
    
    I'm using an add_action in the Plugin implementation instead of embedding it into the run_optin because we don't know which action the dev needs to use for their needs.
    
    I added add_action calls in the enqueue function of the default optin template class because I'm letting the developer decide how to enqueue their styles and scripts. If they don't need to enqueue an actual file, they can just render HTML code like I did.

--- src/DefaultOptinTemplate.php
+++ src/DefaultOptinTemplate.php
@@ -4,11 +4,44 @@ namespace StellarWP\Telemetry;
 
 class DefaultOptinTemplate implements Template {
 
-	public function render() {
-		echo "DefaultOptinTemplate::render()";
+	public function enqueue() {
+		add_action( 'admin_head', [ $this, 'render_styles' ] );
+		add_action( 'admin_head', [ $this, 'render_scripts' ] );
 	}
 
-	public function enqueue() {
-		echo "DefaultOptinTemplate::enqueue()";
+	public function render() {
+		echo <<<HTML
+<div class="stellarwp-telemetry-starter modal">
+<h1>Hello, World</h1>
+</div>
+HTML;
+	}
+
+	public function render_styles() {
+		echo <<<HTML
+<style>
+.stellarwp-telemetry-starter {
+	position: fixed;
+	bottom: 0; 
+	right: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0.5);
+	z-index: 999999;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	transition: all -1.3s ease-in-out;
+}
+</style>
+HTML;
+	}
+
+	public function render_scripts() {
+		echo <<<HTML
+<script>
+alert('Hello World');
+</script>
+HTML;
 	}
 }
