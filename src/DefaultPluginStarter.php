b1c3cf89eb1c80c7cd56faf96f4041167b767595
Rob Gongora <roberto@tri.be>
2022-09-25 18:18:52 +0200

    chore: refactored how to initialize the plugin.

--- src/DefaultPluginStarter.php
+++ src/DefaultPluginStarter.php
@@ -4,12 +4,20 @@ namespace StellarWP\Telemetry;
 
 class DefaultPluginStarter extends PluginStarter {
 
-	public function __construct( ActivationHook $activation_hook, Template $optin_template ) {
-		$this->activation_hook = $activation_hook;
-		$this->optin_template  = $optin_template;
-		$this->plugin_slug     = 'stellarwp_telemetry_starter';
-		$this->plugin_version  = '1.0.0';
-		$this->inherit_option  = 'freemius_inherit';
+	public function __construct( array $args = [] ) {
+		$args                  = wp_parse_args( $args, [
+			'activation_hook' => new DefaultActivationHook(),
+			'optin_template'  => new DefaultOptinTemplate(),
+			'plugin_slug'     => 'stellarwp-telemetry-starter',
+			'plugin_version'  => '1.0.0',
+			'inherit_option'  => 'freemius_inherit',
+		] );
+
+		$this->activation_hook = $args['activation_hook'];
+		$this->optin_template  = $args['optin_template'];
+		$this->plugin_slug     = $args['plugin_slug'];
+		$this->plugin_version  = $args['plugin_version'];
+		$this->inherit_option  = $args['inherit_option'];
 	}
 
 }
