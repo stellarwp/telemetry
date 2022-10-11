dee65f4d898cfd5413757f4eba9cb3ad04d27e5b
Rob Gongora <roberto@tri.be>
2022-10-11 16:00:31 +0200

    feat: refactored to use DI containers.

--- src/DefaultPluginStarter.php
+++ src/DefaultPluginStarter.php
@@ -4,25 +4,13 @@ namespace StellarWP\Telemetry;
 
 class DefaultPluginStarter extends PluginStarter {
 
-	public function __construct( array $args = [] ) {
-		$args = wp_parse_args( $args, [
-			'activation_hook'     => new DefaultActivationHook(),
-			'optin_template'      => new DefaultOptinTemplate(),
-			'plugin_slug'         => 'stellarwp-telemetry-starter',
-			'plugin_version'      => '1.0.0',
-			'activation_redirect' => 'options-general.php?page=stellarwp-telemetry-starter',
-			'telemetry_url'       => getenv( 'STELLARWP_TELEMETRY_URL' ) ?: '',
-		] );
+	public function __construct( ActivationHook $activation_hook, Template $optin_template, TelemetryProvider $provider ) {
+		$this->activation_hook = $activation_hook;
+		$this->optin_template  = $optin_template;
+		$this->provider        = $provider;
+	}
 
-		$this->activation_hook     = $args['activation_hook'];
-		$this->optin_template      = $args['optin_template'];
-		$this->plugin_slug         = $args['plugin_slug'];
-		$this->plugin_version      = $args['plugin_version'];
-		$this->activation_redirect = $args['activation_redirect'];
-		$this->telemetry_url       = $args['telemetry_url'];
-
-		$this->register_cronjob_handlers();
-
+	public function init(): void {
 		add_action( 'admin_init', function () {
 			// We don't want to run this on every ajax request.
 			if ( wp_doing_ajax() ) {
@@ -44,12 +32,8 @@ class DefaultPluginStarter extends PluginStarter {
 					$this->run_optin();
 				}
 			}
-
-			// Add pre update filters for the new options
-			$this->add_pre_update_filters();
 		} );
-
-		// TODO: Add uninstall hook and remove cronjob if exists and option only has current plugin slug as false, or all are false.
 	}
 
+	// TODO: Add uninstall hook and remove cronjob if exists and option only has current plugin slug as false, or all are false.
 }
--- src/PluginStarter.php
+++ src/PluginStarter.php
@@ -3,9 +3,15 @@
 namespace StellarWP\Telemetry;
 
 abstract class PluginStarter {
-	protected const OPTION = 'stellarwp_telemetry';
-	protected const REDIRECT_ON_ACTIVATION = true;
-	protected const CRON_INTERVAL = WEEK_IN_SECONDS;
+	public const OPTION                 = 'stellarwp_telemetry';
+	public const REDIRECT_ON_ACTIVATION = true;
+	public const CRON_INTERVAL          = WEEK_IN_SECONDS;
+	public const PLUGIN_SLUG            = 'stellarwp-telemetry-starter';
+	public const PLUGIN_VERSION         = '1.0.0';
+	public const ACTIVATION_REDIRECT    = 'options-general.php?page=stellarwp-telemetry-starter';
+	public const TELEMETRY_URL          = '';
+	public const YES                    = "1";
+	public const NO                     = "-1";
 
 	/** @var Template $optin_template */
 	protected $optin_template;
@@ -13,26 +19,112 @@ abstract class PluginStarter {
 	/** @var ActivationHook $activation_hook */
 	protected $activation_hook;
 
-	/** @var string $plugin_slug */
-	protected $plugin_slug;
-
-	/** @var string $plugin_version */
-	protected $plugin_version;
-
-	/** @var string $option_to_inherit */
-	protected $option_to_inherit;
-
-	/** @var string $activation_redirect */
-	protected $activation_redirect;
-
-	/** @var string $telemetry_url */
-	protected $telemetry_url;
+	/** @var TelemetryProvider $provider */
+	protected $provider;
 
 	/** @var bool $enqueues_applied */
 	protected $enqueues_applied = false;
 
 	public function activation_hook(): void {
 		$this->activation_hook->run( $this );
+	}
+
+	public function register_cronjob_handlers(): void {
+		add_action( $this->get_cron_hook_name(), function () {
+			if ( empty( $this->get_telemetry_url() ) ) {
+				return;
+			}
+
+			wp_remote_post( $this->get_telemetry_url(), [
+				'headers' => [
+					'Content-Type' => 'application/json',
+				],
+				'body'    => $this->get_telemetry_body(),
+			] );
+		}, 10, 0 );
+	}
+
+	public function get_cron_hook_name() {
+		return apply_filters( 'stellarwp_telemetry_cron_hook_name', $this->get_option_name() . '_cron' );
+	}
+
+	public function get_option_name(): string {
+		return apply_filters( 'stellarwp_telemetry_option_name', self::OPTION );
+	}
+
+	public function get_telemetry_url() {
+		return apply_filters( 'stellarwp_telemetry_url', self::TELEMETRY_URL );
+	}
+
+	public function get_telemetry_body() {
+		return apply_filters( 'stellarwp_telemetry_body', json_encode( [
+			'data' => $this->provider->get_data(),
+		] ) );
+	}
+
+	public function get_plugin_version(): string {
+		return apply_filters( 'stellarwp_telemetry_version', self::PLUGIN_VERSION );
+	}
+
+	public abstract function init(): void;
+
+	public function maybe_add_cronjobs() {
+		if ( $this->get_optin_status() ) {
+			add_action( 'admin_init', function () {
+				// If the cron job is not scheduled, schedule it using as_next_scheduled_action().
+				if ( ! as_next_scheduled_action( $this->get_cron_hook_name() ) ) {
+					as_schedule_recurring_action( time(), $this->get_cron_interval(), $this->get_cron_hook_name() );
+				}
+			} );
+		}
+	}
+
+	public function get_optin_status(): bool {
+		$status = true;
+		$meta   = $this->get_meta();
+
+		// If any plugin's status is false, we should return false.
+		foreach ( $meta as $plugin ) {
+			if ( $plugin['optin'] === false ) {
+				$status = false;
+				break;
+			}
+		}
+
+		return $status;
+	}
+
+	public function get_meta(): array {
+		return (array) get_option( $this->get_option_name(), [] );
+	}
+
+	public function get_cron_interval() {
+		return apply_filters( 'stellarwp_telemetry_cron_interval', self::CRON_INTERVAL );
+	}
+
+	public function is_settings_page(): bool {
+		return ( isset( $_GET['page'] ) && $_GET['page'] === $this->get_plugin_slug() );
+	}
+
+	public function get_plugin_slug(): string {
+		return apply_filters( 'stellarwp_telemetry_plugin_slug', self::PLUGIN_SLUG );
+	}
+
+	public function should_show_optin(): bool {
+		$should_show = get_option( $this->get_show_optin_option_name(), false );
+
+		if ( $should_show === self::YES ) {
+			// Update the option so we don't show the optin again unless something changes this again.
+			update_option( $this->get_show_optin_option_name(), self::NO );
+		}
+
+		$should_show = ( $should_show === self::YES );
+
+		return apply_filters( 'stellarwp_telemetry_should_show_optin', $should_show );
+	}
+
+	public function get_show_optin_option_name(): string {
+		return apply_filters( 'stellarwp_telemetry_show_optin_option_name', $this->get_option_name() . '_show_optin' );
 	}
 
 	public function apply_enqueues(): void {
@@ -51,78 +143,6 @@ abstract class PluginStarter {
 		$this->optin_template->render();
 	}
 
-	public function get_plugin_slug(): string {
-		return apply_filters( 'stellarwp_telemetry_plugin_slug', $this->plugin_slug );
-	}
-
-	public function get_plugin_version(): string {
-		return apply_filters( 'stellarwp_telemetry_version', $this->plugin_version );
-	}
-
-	public function get_option_to_inherit(): string {
-		return apply_filters( 'stellarwp_telemetry_option_to_inherit', $this->option_to_inherit );
-	}
-
-	public function get_option_name(): string {
-		return apply_filters( 'stellarwp_telemetry_option_name', self::OPTION );
-	}
-
-	public function get_meta(): array {
-		return (array) get_option( $this->get_option_name(), [] );
-	}
-
-	public function get_activation_redirect(): string {
-		return apply_filters( 'stellarwp_telemetry_activation_redirect', $this->activation_redirect );
-	}
-
-	public function should_redirect_on_activation(): bool {
-		return (bool) apply_filters( 'stellarwp_telemetry_redirect_on_activation', self::REDIRECT_ON_ACTIVATION );
-	}
-
-	public function get_redirection_option_name(): string {
-		return apply_filters( 'stellarwp_telemetry_redirection_option_name', $this->get_option_name() . '_redirection' );
-	}
-
-	public function get_show_optin_option_name(): string {
-		return apply_filters( 'stellarwp_telemetry_show_optin_option_name', $this->get_option_name() . '_show_optin' );
-	}
-
-	public function get_optin_status_option_name() {
-		return apply_filters( 'stellarwp_telemetry_optin_status_option_name', $this->get_option_name() . '_optin_status' );
-	}
-
-	public function should_show_optin(): bool {
-		$should_show = get_option( $this->get_show_optin_option_name(), false );
-
-		if ( $should_show === "1" ) {
-			// Update the option so we don't show the optin again unless something changes this again.
-			update_option( $this->get_show_optin_option_name(), "-1" );
-		}
-
-		$should_show = ( $should_show === "1" );
-
-		return apply_filters( 'stellarwp_telemetry_should_show_optin', $should_show );
-	}
-
-	public function get_optin_status(): bool {
-		$status = true;
-		$meta   = $this->get_meta();
-
-		// If any plugin's status is false, we should return false.
-		foreach ( $meta as $plugin ) {
-			if ( $plugin['optin'] === false ) {
-				$status = false;
-				break;
-			}
-		}
-
-		return $status;
-	}
-
-	public function is_settings_page(): bool {
-		return ( isset( $_GET['page'] ) && $_GET['page'] === $this->get_plugin_slug() );
-	}
-
 	protected function perform_activation_redirect(): void {
 		if ( $this->should_redirect_on_activation() &&
 		     ! wp_doing_ajax() &&
@@ -134,77 +154,15 @@ abstract class PluginStarter {
 		}
 	}
 
-	public function get_cron_hook_name() {
-		return apply_filters( 'stellarwp_telemetry_cron_hook_name', $this->get_option_name() . '_cron' );
+	public function should_redirect_on_activation(): bool {
+		return (bool) apply_filters( 'stellarwp_telemetry_redirect_on_activation', self::REDIRECT_ON_ACTIVATION );
 	}
 
-	public function get_cron_interval() {
-		return apply_filters( 'stellarwp_telemetry_cron_interval', self::CRON_INTERVAL );
+	public function get_redirection_option_name(): string {
+		return apply_filters( 'stellarwp_telemetry_redirection_option_name', $this->get_option_name() . '_redirection' );
 	}
 
-	public function get_telemetry_url() {
-		return apply_filters( 'stellarwp_telemetry_url', $this->telemetry_url );
+	public function get_activation_redirect(): string {
+		return apply_filters( 'stellarwp_telemetry_activation_redirect', self::ACTIVATION_REDIRECT );
 	}
-
-	public function get_telemetry_body() {
-		// TODO: Use DI to inject the telemetry provider.
-		$provider = new DefaultTelemetryProvider();
-
-		return apply_filters( 'stellarwp_telemetry_body', json_encode( [
-			'data' => $provider->get_data(),
-		] ) );
-	}
-
-	public function maybe_add_cronjobs() {
-		if ( $this->get_optin_status() ) {
-			add_action( 'admin_init', function () {
-				// If the cron job is not scheduled, schedule it using as_next_scheduled_action().
-				if ( ! as_next_scheduled_action( $this->get_cron_hook_name() ) ) {
-					as_schedule_recurring_action( time(), $this->get_cron_interval(), $this->get_cron_hook_name() );
-				}
-			} );
-		}
-	}
-
-	public function register_cronjob_handlers(): void {
-		add_action( $this->get_cron_hook_name(), function () {
-			if ( empty( $this->get_telemetry_url() ) ) {
-				return;
-			}
-
-			wp_remote_post( $this->get_telemetry_url(), [
-				'headers' => [
-					'Content-Type' => 'application/json',
-				],
-				'body'    => $this->get_telemetry_body(),
-			] );
-		}, 10, 0 );
-	}
-
-	protected function add_pre_update_filters() {
-		add_filter( 'pre_update_option_' . $this->get_optin_status_option_name(), function ( $value, $old_value, $option ) {
-			$meta = $this->get_meta();
-
-			// If the value is true, and the old value is false, we want to update the optin status to true on all plugins.
-			if ( $value === "1" && $old_value === "-1" ) {
-				foreach ( $meta as $plugin_slug => $plugin_meta ) {
-					$meta[ $plugin_slug ]['optin'] = true;
-				}
-
-				update_option( $this->get_option_name(), $meta );
-			}
-
-			// If the old value is true, and the new value is false, we want to update the optin status to false on all plugins.
-			if ( $value === "-1" && $old_value === "1" ) {
-				foreach ( $meta as $plugin_slug => $plugin_meta ) {
-					$meta[ $plugin_slug ]['optin'] = false;
-				}
-
-				update_option( $this->get_option_name(), $meta );
-			}
-
-			return $value;
-		}, 10, 3 );
-	}
-
 }
