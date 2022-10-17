72efb155ea3ae40486ecb9b1dc52c7a118c2c40a
Rob Gongora <roberto@tri.be>
2022-10-17 17:18:12 +0200

    feat: added lucatume/di52 as a dev requirement for tests.

--- tests/wpunit/RegisterSiteTest.php
+++ tests/wpunit/RegisterSiteTest.php
@@ -1,33 +1,60 @@
 <?php
 
-class RegisterSiteTest extends \Codeception\TestCase\WPTestCase
-{
-    /**
-     * @var \WpunitTester
-     */
-    protected $tester;
-    
-    public function setUp(): void
-    {
-        // Before...
-        parent::setUp();
+use Codeception\TestCase\WPTestCase;
+use lucatume\DI52\Container;
+use StellarWP\Telemetry\ActivationHook;
+use StellarWP\Telemetry\DefaultActivationHook;
+use StellarWP\Telemetry\DefaultOptinTemplate;
+use StellarWP\Telemetry\DefaultPluginStarter;
+use StellarWP\Telemetry\DefaultTelemetryProvider;
+use StellarWP\Telemetry\PluginStarter;
+use StellarWP\Telemetry\TelemetryProvider;
 
-        // Your set up methods here.
-    }
+class RegisterSiteTest extends WPTestCase {
+	/**
+	 * @var WpunitTester
+	 */
+	protected $tester;
 
-    public function tearDown(): void
-    {
-        // Your tear down methods here.
+	/** @var Container */
+	protected $container;
 
-        // Then...
-        parent::tearDown();
-    }
+	public function setUp(): void {
+		// Before...
+		parent::setUp();
 
-    // Tests
-    public function test_it_works()
-    {
-        $post = static::factory()->post->create_and_get();
-        
-        $this->assertInstanceOf(\WP_Post::class, $post);
-    }
+		// Your set up methods here.
+		$this->container = new Container();
+		$this->container->bind( ActivationHook::class, DefaultActivationHook::class );
+		$this->container->bind( TelemetryProvider::class, DefaultTelemetryProvider::class );
+		$this->container->singleton(
+			PluginStarter::class,
+			function () {
+				return new DefaultPluginStarter(
+					$this->container->get( ActivationHook::class ),
+					new DefaultOptinTemplate(),
+					$this->container->get( TelemetryProvider::class )
+				);
+			}
+		);
+	}
+
+	public function tearDown(): void {
+		// Your tear down methods here.
+
+		// Then...
+		parent::tearDown();
+	}
+
+	// Tests
+	public function test_it_works() {
+		$post = static::factory()->post->create_and_get();
+
+		$this->assertInstanceOf( WP_Post::class, $post );
+
+		$this->assertInstanceOf( DefaultPluginStarter::class, $this->container->get( PluginStarter::class ) );
+	}
+
+	public function test_it_sends_a_request_to_the_register_site_endpoint() {
+	}
 }
