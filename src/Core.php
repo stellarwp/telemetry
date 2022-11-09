0c40e914a5258d5fcc9f5c853d21e65d01359dd6
Tanner Record <tanner.record@gmail.com>
2022-11-09 16:20:13 -0500

    Simplify Core class responsibilities

--- src/Core.php
+++ src/Core.php
@@ -3,10 +3,11 @@
 namespace StellarWP\Telemetry;
 
 class Core {
-	public const PLUGIN_SLUG = 'stellarwp-telemetry-starter';
-	public const PLUGIN_VERSION = '1.0.0';
 	public const YES = "1";
 	public const NO = "-1";
+
+	public string $plugin_slug;
+	public string $plugin_version;
 
 	private static self $instance;
 
@@ -21,7 +22,16 @@ class Core {
 		return self::$instance;
 	}
 
-	public function init(): void {
+	/**
+	 * Sets up the library.
+	 *
+	 * @param string $plugin_path    The path to the main plugin file.
+	 * @param string $plugin_version The current version of the plugin.
+	 */
+	public function init( string $plugin_path, string $plugin_version ): void {
+		$this->plugin_slug    = plugin_basename( $plugin_path );
+		$this->plugin_version = $plugin_version;
+
 		// We don't want to run this on every ajax request.
 		if ( wp_doing_ajax() ) {
 			return;
@@ -30,29 +40,41 @@ class Core {
 		if ( $this->is_settings_page() ) {
 			if ( $this->should_show_optin() ) {
 				// Run optin.
-				$this->run_optin();
+				(new OptInTemplate())->maybe_render();
 			}
 		}
 	}
 
+	/**
+	 * Gets the plugin's version.
+	 */
 	public function get_plugin_version(): string {
-		return apply_filters( 'stellarwp/telemetry/version', self::PLUGIN_VERSION );
+		return $this->plugin_version;
 	}
 
+	/**
+	 * Gets the plugin's slug.
+	 */
+	public function get_plugin_slug(): string {
+		return $this->plugin_slug;
+	}
+
+	/**
+	 * Determines if the current page is the plugin's main settings page.
+	 */
 	public function is_settings_page(): bool {
 		$is_settings_page = ( isset( $_GET['page'] ) && $_GET['page'] === $this->get_plugin_slug() );
 
 		return apply_filters( 'stellarwp/telemetry/is_settings_page', $is_settings_page );
 	}
 
-	public function get_plugin_slug(): string {
-		return apply_filters( 'stellarwp/telemetry/plugin_slug', self::PLUGIN_SLUG );
-	}
-
+	/**
+	 * Determines if the optin modal should be shown to the user.
+	 */
 	public function should_show_optin(): bool {
-		$should_show = get_option( $this->get_show_optin_option_name(), false );
+		$should_show = (bool) get_option( $this->get_show_optin_option_name(), false );
 
-		if ( $should_show === self::YES ) {
+		if ( $should_show ) {
 			// Update the option so we don't show the optin again unless something changes this again.
 			update_option( $this->get_show_optin_option_name(), self::NO );
 		}
@@ -62,11 +84,10 @@ class Core {
 		return apply_filters( 'stellarwp/telemetry/should_show_optin', $should_show );
 	}
 
+	/**
+	 * Gets the optin option name.
+	 */
 	public function get_show_optin_option_name(): string {
 		return apply_filters( 'stellarwp/telemetry/show_optin_option_name', ( new OptInStatus() )->get_option_name() . '_show_optin' );
-	}
-
-	public function run_optin(): void {
-		( new OptInTemplate() )->render();
 	}
 }
