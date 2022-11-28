6f243d163e6ccf62fa2c89409a362905bd9b7ed3
Tanner Record <tanner.record@gmail.com>
2022-11-28 15:50:41 -0500

    Set up response to match requirements

--- src/Telemetry/Routes/Check.php
+++ src/Telemetry/Routes/Check.php
@@ -7,7 +7,7 @@ use WP_REST_Response;
 class Check {
 
     public function response() {
-		$response = new WP_REST_Response();
+		$response = new WP_REST_Response( [ 'results' => 'success' ] );
 		$response->set_status( 200 );
 
 		return $response;
