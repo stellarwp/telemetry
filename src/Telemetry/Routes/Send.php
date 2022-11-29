3649238d522f2dbcce49a6b01a2c4b17a2e67441
Tanner Record <tanner.record@gmail.com>
2022-11-29 12:55:28 -0500

    Send data to telemetry server

--- src/Telemetry/Routes/Send.php
+++ src/Telemetry/Routes/Send.php
@@ -2,6 +2,7 @@
 
 namespace StellarWP\Telemetry\Routes;
 
+use StellarWP\Telemetry\Config;
 use StellarWP\Telemetry\Telemetry;
 use WP_REST_Request;
 
@@ -17,6 +18,8 @@ class Send extends Abstract_Route {
 		if ( ! wp_verify_nonce( $request->get_params( 'nonce' ), Telemetry::NONCE ) ) {
 			$this->send_early_unauthorized();
 		}
+
+		Config::get_container()->get( Telemetry::class )->send_data();
 
 		$this->send_early_ok();
 	}
