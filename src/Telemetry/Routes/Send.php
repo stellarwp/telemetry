171a5f7f0fe6856671dada6ab7090c57c69b3e3a
Tanner Record <tanner.record@gmail.com>
2022-11-29 12:28:55 -0500

    Set up nonce verification for send route

--- src/Telemetry/Routes/Send.php
+++ src/Telemetry/Routes/Send.php
@@ -2,6 +2,9 @@
 
 namespace StellarWP\Telemetry\Routes;
 
+use StellarWP\Telemetry\Telemetry;
+use WP_REST_Request;
+
 class Send extends Abstract_Route {
 
     public function get_endpoint() {
@@ -9,6 +12,12 @@ class Send extends Abstract_Route {
 	}
 
     public function action() {
+		$request = new WP_REST_Request();
+
+		if ( ! wp_verify_nonce( $request->get_params( 'nonce' ), Telemetry::NONCE ) ) {
+			$this->send_early_unauthorized();
+		}
+
 		$this->send_early_ok();
 	}
 }
