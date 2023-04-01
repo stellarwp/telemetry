1ae70270a4fa350a24bd567f323ca6cb37db0828
Tanner Record <tanner.record@gmail.com>
2023-03-31 21:14:05 -0400

    Output assets if any stellar_slug should render

--- src/Telemetry/Admin/Admin_Subscriber.php
+++ src/Telemetry/Admin/Admin_Subscriber.php
@@ -9,6 +9,7 @@
 
 namespace StellarWP\Telemetry\Admin;
 
+use StellarWP\Telemetry\Config;
 use StellarWP\Telemetry\Contracts\Abstract_Subscriber;
 use StellarWP\Telemetry\Opt_In\Opt_In_Template;
 
@@ -37,13 +38,24 @@ class Admin_Subscriber extends Abstract_Subscriber {
 	 * Registers required hooks to set up the admin assets.
 	 *
 	 * @since 1.0.0
+	 * @since 2.0.0 - Adjust to output assets if any stellar slug should render its modal.
 	 *
 	 * @return void
 	 */
 	public function maybe_enqueue_admin_assets() {
 		global $pagenow;
 
-		if ( 'plugins.php' === $pagenow || $this->container->get( Opt_In_Template::class )->should_render() ) {
+		$should_render = false;
+
+		foreach ( Config::get_all_stellar_slugs() as $stellar_slug ) {
+			$should_render = $this->container->get( Opt_In_Template::class )->should_render( $stellar_slug );
+
+			if ( $should_render ) {
+				break;
+			}
+		}
+
+		if ( 'plugins.php' === $pagenow || $should_render ) {
 			$this->container->get( Resources::class )->enqueue_admin_assets();
 		}
 	}
