97f5fd737647ffac8aa4fabd9f9356915be291a2
Tanner Record <tanner.record@gmail.com>
2023-04-03 09:55:17 -0400

    Refactor exit interview modal filtering

--- src/Telemetry/Exit_Interview/Template.php
+++ src/Telemetry/Exit_Interview/Template.php
@@ -48,52 +48,71 @@ class Template implements Template_Interface {
 	 * Gets the arguments for configuring the "Exit Interview" modal.
 	 *
 	 * @since 1.0.0
+	 * @since 2.0.0
+	 *
+	 * @param string $stellar_slug The stellar slug used when outputting the modal.
 	 *
 	 * @return array
 	 */
-	protected function get_args() {
+	protected function get_args( string $stellar_slug ) {
+
+		$args = [
+			'plugin_slug'        => $stellar_slug,
+			'plugin_logo'        => Resources::get_asset_path() . 'resources/images/stellar-logo.svg',
+			'plugin_logo_width'  => 151,
+			'plugin_logo_height' => 32,
+			'plugin_logo_alt'    => 'StellarWP Logo',
+			'heading'            => __( 'We’re sorry to see you go.', 'stellarwp-telemetry' ),
+			'intro'              => __( 'We’d love to know why you’re leaving so we can improve our plugin.', 'stellarwp-telemetry' ),
+			'uninstall_reasons'  => [
+				[
+					'uninstall_reason_id' => 'confusing',
+					'uninstall_reason'    => __( 'I couldn’t understand how to make it work.', 'stellarwp-telemetry' ),
+				],
+				[
+					'uninstall_reason_id' => 'better-plugin',
+					'uninstall_reason'    => __( 'I found a better plugin.', 'stellarwp-telemetry' ),
+				],
+				[
+					'uninstall_reason_id' => 'no-feature',
+					'uninstall_reason'    => __( 'I need a specific feature it doesn’t provide.', 'stellarwp-telemetry' ),
+				],
+				[
+					'uninstall_reason_id' => 'broken',
+					'uninstall_reason'    => __( 'The plugin doesn’t work.', 'stellarwp-telemetry' ),
+				],
+				[
+					'uninstall_reason_id' => 'other',
+					'uninstall_reason'    => __( 'Other', 'stellarwp-telemetry' ),
+					'show_comment'        => true,
+				],
+			],
+		];
+
 		/**
 		 * Filters the "Exit Interview" modal arguments.
 		 *
 		 * @since 1.0.0
+		 * @since 2.0.0 - Added the current stellar slug.
 		 *
-		 * @param array $args The arguments used to configure the modal.
+		 * @param array  $args         The arguments used to configure the modal.
+		 * @param string $stellar_slug The current stellar slug for the plugin outputting the modal.
 		 */
-		return apply_filters(
-			'stellarwp/telemetry/' . Config::get_stellar_slug() . '/exit_interview_args',
-			[
-				'plugin_slug'        => Config::get_stellar_slug(),
-				'plugin_logo'        => Resources::get_asset_path() . 'resources/images/stellar-logo.svg',
-				'plugin_logo_width'  => 151,
-				'plugin_logo_height' => 32,
-				'plugin_logo_alt'    => 'StellarWP Logo',
-				'heading'            => __( 'We’re sorry to see you go.', 'stellarwp-telemetry' ),
-				'intro'              => __( 'We’d love to know why you’re leaving so we can improve our plugin.', 'stellarwp-telemetry' ),
-				'uninstall_reasons'  => [
-					[
-						'uninstall_reason_id' => 'confusing',
-						'uninstall_reason'    => __( 'I couldn’t understand how to make it work.', 'stellarwp-telemetry' ),
-					],
-					[
-						'uninstall_reason_id' => 'better-plugin',
-						'uninstall_reason'    => __( 'I found a better plugin.', 'stellarwp-telemetry' ),
-					],
-					[
-						'uninstall_reason_id' => 'no-feature',
-						'uninstall_reason'    => __( 'I need a specific feature it doesn’t provide.', 'stellarwp-telemetry' ),
-					],
-					[
-						'uninstall_reason_id' => 'broken',
-						'uninstall_reason'    => __( 'The plugin doesn’t work.', 'stellarwp-telemetry' ),
-					],
-					[
-						'uninstall_reason_id' => 'other',
-						'uninstall_reason'    => __( 'Other', 'stellarwp-telemetry' ),
-						'show_comment'        => true,
-					],
-				],
-			]
-		);
+		$args = apply_filters( 'stellarwp/telemetry/' . $stellar_slug . '/exit_interview_args', $args, $stellar_slug );
+
+		/**
+		 * A more general filter to filter the "Exit Interview" modal arguments.
+		 *
+		 * @since 2.0.0
+		 *
+		 * @param array  $args         The arguments used to configure the modal.
+		 * @param string $stellar_slug The current stellar slug for the plugin outputting the modal.
+		 *
+		 * @return void
+		 */
+		$args = apply_filters( 'stellarwp/telemetry/exit_interview_args', $args, $stellar_slug );
+
+		return $args;
 	}
 
 	/**
